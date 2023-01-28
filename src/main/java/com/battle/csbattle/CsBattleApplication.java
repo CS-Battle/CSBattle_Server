4e38a024483d7159e078f8cb34670939b0d33e71
Jimin Chung <chung.jimin@mintech.kr>
2023-01-28 21:59:48 +0900

    initialize spring project
    Signed-off-by: Jimin Chung <chung.jimin@mintech.kr>

---
+++ src/main/java/com/battle/csbattle/CsBattleApplication.java
@@ -0,0 +1,13 @@
+package com.battle.csbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/battle/csbattle/CsBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battle.csbattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
