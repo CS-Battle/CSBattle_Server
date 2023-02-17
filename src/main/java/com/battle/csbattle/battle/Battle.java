935642f9ef24b983a598b656652ac314602831b3
Jimin Chung <chung.jimin@mintech.kr>
2023-02-18 02:10:01 +0900

    🔨 feat: #1 add question repository & service code - question repository 작성 - question service 함수 틀 & 역할 작성

--- src/main/java/com/battle/csbattle/battle/Battle.java
+++ src/main/java/com/battle/csbattle/battle/Battle.java
@@ -15,7 +15,7 @@ public class Battle {
     private String id;                                                  // 배틀 id
     private BattleType type;                                            // 배틀 유형
     private Map<String, SseEmitter> players = new HashMap<>();          // 참여 중인 플레이어
-    private List<QuestionDto> questions = new ArrayList<>();            // 이 배틀에서의 문제 목록 (배틀 참여자들은 모두 동일한 문제를 풀어야 하기에 필요) TODO: 추후 DB 연결 시 question id 리스트로 관리
+    private List<QuestionDto> questions = new ArrayList<>();            // 이 배틀에서의 문제 목록 (배틀 참여자들은 모두 동일한 문제를 풀어야 하기에 필요)
 
     public static Battle create(BattleType type, Map<String, SseEmitter> clients) {
         Battle battle = new Battle();
--- src/main/java/com/battle/csbattle/service/QuestionService.java
+++ src/main/java/com/battle/csbattle/service/QuestionService.java
@@ -3,19 +3,31 @@ package com.battle.csbattle.service;
 import com.battle.csbattle.battle.Battle;
 import com.battle.csbattle.dto.AnswerDto;
 import com.battle.csbattle.dto.QuestionDto;
+import com.battle.csbattle.entity.Question;
+import com.battle.csbattle.entity.QuestionType;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @NoArgsConstructor
 public class QuestionService {
+
+    // TODO: 함수 작성
+    public QuestionDto getOneQuestionByQuestionType(QuestionType questionType) {
+        QuestionDto questionDto = new QuestionDto();
+
+        return questionDto;
+    }
+
+    // TODO: 1. getOneQuestion -> getQuestion 으로 변경
+    //       2. battle 뒤에 인자로 count 추가해서 count 개만큼 문제 불러오는 함수로 변경
     public QuestionDto getOneQuestion(Battle battle) {              // 한문제배틀 문제 불러오기 (해당 battle 에 대해 출제해줄 문제 선정)
         QuestionDto returningQuestion;
 
         if (battle.getQuestions().isEmpty()) {
             System.out.println("*** 이 배틀의 문제 생성");
 
-            QuestionDto question = QuestionDto.builder()           // 문제 DB 에서 문제 한개 불러오기 - 현재는 DB가 없기에, QuestionDto 를 생성해주는것으로 대신
+            QuestionDto question = QuestionDto.builder()           // TODO: 문제 DB (QuestionRepository)에서 문제 한개 불러오기 - 현재는 DB가 없기에, QuestionDto 를 생성해주는것으로 대신
                     .questionId("question1")
                     .content("문제를 풀어봐요")
                     .answer("답")
@@ -33,7 +45,7 @@ public class QuestionService {
     }
 
     public Boolean checkAnswer(Battle battle, AnswerDto answer) {        // 정답 여부 확인
-        QuestionDto question = battle.getQuestions().get(0);
+        QuestionDto question = battle.getQuestions().get(0);            // TODO: 여기 변경
 
         Boolean isCorrect = false;
         if(question.getAnswer().equals(answer.getAnswer())){ isCorrect = true; }
