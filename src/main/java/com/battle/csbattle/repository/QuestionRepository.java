315a9f74c24c790ef62173e8a303cd55e4df3b5e
Jansoon <ckdals4600@naver.com>
2023-03-02 01:50:18 +0900

    🔨 feat: #3 Add method to QuestionRepository - 테이블에서 랜덤하게 1개의 항목만 가져오는 findRandomOne 메소드 추가 - 모든 항목을 가져오는 findAll 메서드 추가

--- src/main/java/com/battle/csbattle/repository/QuestionRepository.java
+++ src/main/java/com/battle/csbattle/repository/QuestionRepository.java
@@ -1,7 +1,9 @@
 package com.battle.csbattle.repository;
 
+import com.battle.csbattle.dto.QuestionDto;
 import com.battle.csbattle.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,8 @@ import java.util.Optional;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     Optional<Question> findById(Long Id);
     Long countBy();
+    @Query(value = "SELECT * FROM Question order by RAND() limit 1",nativeQuery = true)
+    Question findRandomOne();
+
+    List<Question> findAll();
 }
