6a4910b9b38558e1425e4fe8df2981323299ce63
goder-0 <goder0@naver.com>
2023-03-19 03:14:50 +0900

    ✨feat: #9 Create User Connection SSE Scheduler - 각 유저에게 SSE 연결 상태에 대한 확인을 위해 SseEmitter 객체에 더미 데이터를 전송함. - 현재는 1분 단위로 실행

---
+++ src/main/java/com/battle/csbattle/util/Scheduler.java
@@ -0,0 +1,26 @@
+package com.battle.csbattle.util;
+
+import com.battle.csbattle.battle.Battle;
+import com.battle.csbattle.dto.UserDto;
+import com.battle.csbattle.service.SseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+
+@Component
+public class Scheduler {
+    @Async
+    @Scheduled(fixedDelay = 1000*60) //60초
+    public void schedulerFixedDelayTask() {
+        Map<String,UserDto> allPlayers= SseService.getAllPlayers();
+        System.out.println(System.currentTimeMillis()/1000);
+        for(String key:allPlayers.keySet()){
+            SseUtil.sendToClient(allPlayers.get(key).getEmitter(),"Connecting Check","Connecting Check");
+        }
+    }
+}
