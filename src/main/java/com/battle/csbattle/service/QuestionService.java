5ab662231d6ac0e909a6a3293a6cc91363f3b825
Jansoon <ckdals4600@naver.com>
2023-03-19 19:14:07 +0900

    :art: feat: #14 Delete getOneQuestion, edit oneQuestionAndResponse - 인덱스에 따른 문제 호출로 인해 getOneQuestion 기능이 중복 되어 삭제 - oneQuestionAndResponse 문제 호출 부분을 Repository 에서 직접 가져오던 방식에서 인덱스에 따라 가져오도록 수정

--- src/main/java/com/battle/csbattle/service/QuestionService.java
+++ src/main/java/com/battle/csbattle/service/QuestionService.java
@@ -4,13 +4,13 @@ import com.battle.csbattle.battle.Battle;
 import com.battle.csbattle.battle.BattleStatus;
 import com.battle.csbattle.dto.AnswerDto;
 import com.battle.csbattle.dto.QuestionDto;
-import com.battle.csbattle.dto.UserDto;
 import com.battle.csbattle.entity.Question;
 import com.battle.csbattle.entity.QuestionType;
 import com.battle.csbattle.repository.QuestionRepository;
 import com.battle.csbattle.util.SseUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -30,11 +30,10 @@ public class QuestionService {
 
     public String oneQuestionAndResponse(Battle battle,String userId){
         battle.setBattleStatus(BattleStatus.AbleAnswer);
-        QuestionDto questionDto=QuestionDto.from(questionRepository.findRandomOne());
-//        for(String key:battle.getPlayers().keySet()){
-//            SseUtil.sendToClient(battle.getPlayers().get(key).getEmitter(),"Question",questionDto);
-//        }
+
+        QuestionDto questionDto = getQuestionByUserIndex(battle, userId);
         SseUtil.sendToClient(battle.getPlayers().get(userId).getEmitter(),"Question",questionDto);
+
         try {
             Thread.sleep(1000*10);
         }catch (InterruptedException e){
@@ -43,23 +42,12 @@ public class QuestionService {
         battle.setBattleStatus(BattleStatus.Gaming);
         return "제한시간이 만료되었습니다.";
     }
-    public QuestionDto getOneQuestion(Battle battle) {              // 한 문제 불러오기 (해당 battle 에 대해 출제해줄 문제 선정)
-        List<QuestionDto> battleQuestion = battle.getQuestions();
-        List<Question> questions = questionRepository.findAll();
 
-        Random random = new Random();
-        int index = random.nextInt(questions.size());
-
-        QuestionDto returnQuestion;
-        if(battleQuestion.size() == 0) {
-            returnQuestion = QuestionDto.from(questions.get(index));
-            battle.addQuestion(returnQuestion);
-        }
-        else {
-            returnQuestion = battleQuestion.get(0);
-        }
-
-        return returnQuestion;
+    public QuestionDto getQuestionByUserIndex(Battle battle, String userId){
+        Integer index = battle.getOngoingQuestions().get(userId);
+        List<QuestionDto> questions = battle.getQuestions();
+        QuestionDto questionDto = questions.get(index);
+        return questionDto;
     }
 
     public void addQuestionsToBattle(Battle battle, int count){
